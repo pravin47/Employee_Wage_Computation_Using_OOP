51cdafc14ed201aa7006bdc2e3d574b0cb008eaa
pravin47 <bagul.pravin2@gmail.com>
2021-02-04 22:19:13 +0530

    Addet:Emp_Check_Present_Absent

---
+++ Emp_Wage_OOP.java
@@ -0,0 +1,20 @@
+package com.Emp_Wage;
+
+public class Emp_Wage_OOP {
+
+	public static void Emp_Check() {
+
+		int empType = (int) (Math.random() * 10) % 2;
+
+		if (empType == 1) {
+
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
+
+	}
+         public static void main(String args[]) {
+		Emp_Check();
+	}
+}
