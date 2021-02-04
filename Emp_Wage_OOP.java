cfec4f7cb82c5095d8daa80a61449d21836ac54d
pravin47 <bagul.pravin2@gmail.com>
2021-02-04 23:11:42 +0530

    Added:Add_Part_Time

--- Emp_Wage_OOP.java
+++ Emp_Wage_OOP.java
@@ -1,30 +1,42 @@
 package com.Emp_Wage;
-import java.io.*;
+
 class Emp_Daily_Wage {
+
 	public static void Daily_Wage() {
 		int Wage_Per_Hr = 20;
 		int Full_Day_Hr = 8;
 		int Wage = Wage_Per_Hr * Full_Day_Hr;
 		System.out.println("Employee Daily Wage is :" + Wage);
 	}
+
+	public static void Part_Time() {
+		int Part_time_hr = 8;
+		int Part_Time_Wage = 20;
+		int partTimeWage = Part_time_hr * Part_Time_Wage;
+		System.out.println("Employee Part time Wage is :" + partTimeWage);
+	}
 }
+
 public class Emp_Wage_OOP extends Emp_Daily_Wage {
 
 	public static void Emp_Check() {
 
-		int empType = (int) (Math.random() * 10) % 2;
+		int empType = (int) (Math.random() * 10) % 3;
 
 		if (empType == 1) {
-
 			System.out.println("Employee is present");
 			Daily_Wage();
+		} else if (empType == 2) {
+			System.out.println("Employee is part time");
+			Part_Time();
 		} else {
 			System.out.println("Employee is absent");
 		}
-
 	}
-         public static void main(String args[]) {
+
+	public static void main(String args[]) {
 		Emp_Check();
 	}
+
 }
 
