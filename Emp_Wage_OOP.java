229fe4039d568accbe760e23a9dfddd74cba8cf6
pravin47 <bagul.pravin2@gmail.com>
2021-02-04 23:53:12 +0530

    Added:Wage_For_Month

--- Emp_Wage_OOP.java
+++ Emp_Wage_OOP.java
@@ -5,16 +5,22 @@ class Emp_Daily_Wage {
 	public static void Full_Time() {
 		int Wage_Per_Hr = 20;
 		int Full_Day_Hr = 8;
+		int Working_Day = 20;
 		int Wage = Wage_Per_Hr * Full_Day_Hr;
+		int Wage_per_Month = ((Wage_Per_Hr * Full_Day_Hr) * Working_Day);
 		System.out.println("Employee is Present ");
 		System.out.println("Employee Daily Wage is :" + Wage);
+		System.out.println("Employee monthly Wage is :" + Wage_per_Month);
 	}
 
 	public static void Part_Time() {
 		int Part_time_hr = 8;
 		int Part_Time_Wage = 20;
+		int Working_Day = 20;
 		int partTimeWage = Part_time_hr * Part_Time_Wage;
+		int Wage_per_Month = ((Part_time_hr * Part_Time_Wage) * Working_Day);
 		System.out.println("Employee Part time Wage is :" + partTimeWage);
+		System.out.println("Employee monthly Wage is :" + Wage_per_Month);
 	}
 
 	public static void Absent() {
